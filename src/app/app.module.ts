0501c012c5bc9014c3aa3d0c9029f83851b6cfe8
Toshio Minei <sminei10@gmail.com>
2018-08-30 14:52:17 -0500

    componentes de preguntas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//Materialo Angular
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import 'hammerjs';
+
+//Components
+import { QuestionDetailComponent } from './question/question-detail.component';
+
+//Moment
+import { MomentModule } from 'ngx-moment';
+import 'moment/locale/es';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MomentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
