b5bad50186509244421b24b315ffd34c201c55e2
Toshio Minei <sminei10@gmail.com>
2018-09-12 04:01:46 -0500

    creación de modelos con mongoose

---
+++ server/models/answer.js
@@ -0,0 +1,8 @@
+import mongoose, { Schema } from 'mongoose'
+
+const AnswerSchema = new Schema({
+  description: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now, required: true }
+})
+
+export default mongoose.model('Answer', AnswerSchema)
