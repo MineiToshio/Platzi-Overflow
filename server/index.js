7dbc08baf1886bcd2279a4f3ad661d00345822b3
Toshio Minei <sminei10@gmail.com>
2018-09-13 12:49:23 -0500

    Creando el build para producción

--- server/index.js
+++ server/index.js
@@ -2,17 +2,16 @@ import http from 'http'
 import Debug from 'debug'
 import app from './app'
 import mongoose from 'mongoose'
-import { mongoUrl } from './config'
+import { mongoUrl, port } from './config'
 
-const PORT = 3000
 const debug = new Debug('platzi-overflow:root')
 
 async function start () {
   mongoose.set('useCreateIndex', true)
   await mongoose.connect(mongoUrl, { useNewUrlParser: true })
 
-  app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`)
+  app.listen(port, () => {
+    console.log(`Server running at port ${port}`)
   })
 }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://calm-brook-51390.herokuapp.com/api'
 };
